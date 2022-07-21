bfd30646f02c84bd3b3591b895634c996f9b88c8
smlee97 <smlee@pentasecurity.com>
2022-07-21 09:53:03 +0900

    add Frontend file code

--- frontend/src/config/listEventConfig.tsx
+++ frontend/src/config/listEventConfig.tsx
@@ -18,3 +18,36 @@ export const categoryConfig = [
     label: 'list_event.category.communication',
   },
 ];
+
+export const listEventTableConfig: IListEventTable[] = [
+  {
+    key: 'category',
+    label: 'Event Category',
+    render: ({ category }) => <>{t(getCategoryConfig(category)?.label || '')}</>,
+    rawContent: ({ category }) => t(getCategoryConfig(category)?.label || ''),
+  },
+  {
+    key: 'datetime',
+    label: 'Date/Time',
+    render: ({ createdAt }) => <>{dayjs(createdAt).format('hh:mm a DD/MM/YYYY')}</>,
+    rawContent: ({ createdAt }) => dayjs(createdAt).format('hh:mm a DD/MM/YYYY'),
+  },
+  { key: 'sendNode', label: 'Send Node' },
+  { key: 'receiveNode', label: 'Receive Node' },
+  { key: 'detectionNode', label: 'Detection Node' },
+  {
+    key: 'eventType',
+    label: 'Event type',
+    render: ({ eventType }) =>
+      eventType ? <span className="list-event__eventType">{eventType.toString()}</span> : <></>,
+  },
+  { key: 'status', label: 'Status' },
+  { key: 'request', label: 'Request' },
+  { key: 'action', label: 'Action' },
+  {
+    key: 'eventInfo',
+    label: 'Event Information',
+    render: (data) => renderEventInfo(data),
+    rawContent: (data) => getEventInfoTextOnly(data),
+  },
+];
