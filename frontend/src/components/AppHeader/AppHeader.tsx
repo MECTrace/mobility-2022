d947313c5ce5a1e69d138ebc9e95260f3db0072f
smlee97 <smlee@pentasecurity.com>
2022-07-25 12:53:10 +0900

    add AppHeader file

---
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { ActionIcon, Group, Header, Tooltip, useMantineColorScheme } from '@mantine/core';
+
+import { defaultLanguage, languageConfig } from '@/config/system';
+import i18n from '@/config/i18n';
+import { Path } from '@/config/path';
+
+import { Sun, MoonStars } from 'tabler-icons-react';
+import { ReactComponent as Logo } from '@/assets/icons/Logo_dark.svg';
+import { ReactComponent as LogoDark } from '@/assets/icons/Logo.svg';
+
+import './AppHeader.scss';
+
+export const AppHeader = () => {
+    const { t } = useTranslation();
+
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+    const isDarkMode = colorScheme === 'dark';
+  
+    const [toggleLang, setToggleLang] = useState(
+      localStorage.getItem('i18nextLng') || defaultLanguage,
+    );
+};
