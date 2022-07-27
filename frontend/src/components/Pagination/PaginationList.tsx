34bf369adaada458dd3463c37ae00950c7a23b3f
smlee97 <smlee@pentasecurity.com>
2022-07-27 14:31:21 +0900

    add PaginationList file

---
+++ frontend/src/components/Pagination/PaginationList.tsx
@@ -0,0 +1,26 @@
+import { paginationConfig } from '@/config/system';
+import { IPaginationData } from '@/interface/interfaceCommon';
+import { renderPaginationInfo } from '@/util/common';
+import { safeAnyToNumber } from '@/util/primitiveHandle';
+import { Loader, Pagination, Select } from '@mantine/core';
+import { UseForm } from '@mantine/hooks/lib/use-form/use-form';
+import { Dispatch, SetStateAction } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import './PaginationList.scss';
+
+export const PaginationList = <T extends IPaginationData>({
+  fetchingState,
+  paginationData,
+  filterForm,
+}: {
+  fetchingState: {
+    isFetching: boolean;
+    setIsFetching: Dispatch<SetStateAction<boolean>>;
+  };
+  paginationData?: IPaginationData;
+  filterForm: UseForm<T>;
+}) => {
+  const { t } = useTranslation();
+  
+};
