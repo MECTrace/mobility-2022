3eaf8edbaf828e63fbeee67614d531da7a5e596c
smlee97 <smlee@pentasecurity.com>
2022-08-02 13:06:03 +0900

    add listEventHelper file

---
+++ frontend/src/helper/listEventHelper.tsx
@@ -0,0 +1,20 @@
+import { Tooltip } from '@mantine/core';
+import dayjs from 'dayjs';
+import { t } from 'i18next';
+
+import { categoryConfig, listEventTableConfig } from '@/config/listEventConfig';
+import { CPUThreshold, NICThreshold, paginationConfig, RAMThreshold } from '@/config/system';
+import {
+  IEventInfoAvailability,
+  IEventInfoCommunication,
+  IEventInfoVirus,
+  IListEvent,
+  IListEventForm,
+  IListEventReq,
+} from '@/interface/interfaceListEvent';
+import { TypesOf } from '@/interface/interfaceCommon';
+import { checkStrArrIncludeKeyword } from '@/util/arrayHandle';
+import { safeAnyToNumber } from '@/util/primitiveHandle';
+
+export const getCategoryConfig = (category: TypesOf<IListEvent>) =>
+  categoryConfig.find(({ id }) => id === category);
