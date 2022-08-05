cfb30f677383f886fa22f91e32acf4410fc5cdf8
smlee97 <smlee@pentasecurity.com>
2022-08-05 13:01:39 +0900

    add interface file

---
+++ frontend/src/interface/interfaceCommon.ts
@@ -0,0 +1,14 @@
+import { Path } from '@/config/path';
+import { LazyExoticComponent } from 'react';
+import { Socket } from 'socket.io-client';
+import { IDiagramData } from './interfaceHierarchyTree';
+
+/**
+ * Get all 'values' of `T` interface
+ */
+export type TypesOf<T> = T[keyof T];
+
+export interface IRoute {
+  path: Path;
+  component: LazyExoticComponent<() => JSX.Element>;
+}   
