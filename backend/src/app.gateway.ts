46496f5ec93e109c8d3e5c540ef8a15086d0154f
smlee97 <smlee@pentasecurity.com>
2022-09-13 09:07:41 +0900

    add file : src_gateway

---
+++ backend/src/app.gateway.ts
@@ -0,0 +1,25 @@
+import {
+    SubscribeMessage,
+    WebSocketGateway,
+    OnGatewayInit,
+    WebSocketServer,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+  } from '@nestjs/websockets';
+  import { Logger } from '@nestjs/common';
+  import { Socket, Server } from 'socket.io';
+  import { EventService } from './modules/event/service/event.service';
+  import { ConfigService } from '@nestjs/config';
+  import { CategoryEnum } from './modules/event/enum/category.enum';
+  import { EventDataDto } from './modules/event/dto/event-data.dto';
+  import { RsuService } from './modules/node/service/rsu.service';
+  import { Event } from './modules/event/entity/event.entity';
+  import 'dotenv/config';
+  
+  @WebSocketGateway({
+    cors: {
+      origin: process.env.FE_URL ? process.env.FE_URL.split(',') : '*',
+      credentials: true,
+      preflightContinue: false,
+    },
+  })
