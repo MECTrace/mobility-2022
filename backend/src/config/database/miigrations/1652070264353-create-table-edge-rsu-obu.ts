693ad868bd87f9d4570121a9db3b5fcc55e49af4
smlee97 <smlee@pentasecurity.com>
2022-09-14 13:21:39 +0900

    add code : obu

--- backend/src/config/database/miigrations/1652070264353-create-table-edge-rsu-obu.ts
+++ backend/src/config/database/miigrations/1652070264353-create-table-edge-rsu-obu.ts
@@ -38,3 +38,60 @@ import {
         }),
         true,
       );
+      
+      await queryRunner.createTable(
+        new Table({
+          name: 'rsu',
+          columns: [
+            {
+              name: 'id',
+              type: 'int',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'name',
+              isNullable: false,
+              isUnique: true,
+              type: 'varchar',
+            },
+            {
+              name: 'cpu',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'ram',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'tx',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'rx',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'edgeId',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'createdAt',
+              type: 'timestamptz',
+              default: 'now()',
+            },
+            {
+              name: 'updatedAt',
+              type: 'timestamptz',
+              default: 'now()',
+            },
+          ],
+        }),
+        true,
+      );
