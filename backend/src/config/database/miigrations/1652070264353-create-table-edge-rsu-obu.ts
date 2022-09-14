e2375cc637e155b35275bf7b58b1aaa9444f7e3e
smlee97 <smlee@pentasecurity.com>
2022-09-14 18:54:59 +0900

    add code : obu

--- backend/src/config/database/miigrations/1652070264353-create-table-edge-rsu-obu.ts
+++ backend/src/config/database/miigrations/1652070264353-create-table-edge-rsu-obu.ts
@@ -95,3 +95,69 @@ import {
         }),
         true,
       );
+      
+    await queryRunner.createForeignKey(
+        'rsu',
+        new TableForeignKey({
+          columnNames: ['edgeId'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'edge',
+          onDelete: 'CASCADE',
+        }),
+      );
+  
+      await queryRunner.createTable(
+        new Table({
+          name: 'obu',
+          columns: [
+            {
+              name: 'id',
+              type: 'int',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment',
+            },
+            {
+              name: 'name',
+              isNullable: false,
+              isUnique: true,
+              type: 'varchar',
+            },
+            {
+              name: 'rsuId',
+              isNullable: false,
+              type: 'int',
+            },
+            {
+              name: 'createdAt',
+              type: 'timestamptz',
+              default: 'now()',
+            },
+            {
+              name: 'updatedAt',
+              type: 'timestamptz',
+              default: 'now()',
+            },
+          ],
+        }),
+        true,
+      );
+  
+      await queryRunner.createForeignKey(
+        'obu',
+        new TableForeignKey({
+          columnNames: ['rsuId'],
+          referencedColumnNames: ['id'],
+          referencedTableName: 'rsu',
+          onDelete: 'CASCADE',
+        }),
+      );
+    }
+  
+    async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('obu');
+      await queryRunner.dropTable('rsu');
+      await queryRunner.dropTable('edge');
+    }
+  }
+  
