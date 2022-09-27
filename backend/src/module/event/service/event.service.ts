ed759c6e16157bb26022891ee775b822a1093e83
smlee97 <smlee@pentasecurity.com>
2022-09-27 14:45:58 +0900

    add file : event_service

---
+++ backend/src/module/event/service/event.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RsuService } from '../../../modules/node/service/rsu.service';
+import {
+  Brackets,
+  DeleteResult,
+  LessThan,
+  Repository,
+  SelectQueryBuilder,
+  WhereExpressionBuilder,
+} from 'typeorm';
+import { EventDataDto } from '../dto/event-data.dto';
+import { QueryLoadMoreEventBodyDto } from '../dto/query-load-more-event-body.dto';
+import { QueryPagingEventBodyDto } from '../dto/query-paging-event-body.dto';
+import { Event } from '../entity/event.entity';
+import { CategoryEnum } from '../enum/category.enum';
+import { PaginationEventDto } from '../dto/pagination-event.dto';
+import { LoadMoreEventDto } from '../dto/load-more-event.dto';
+import { QuerySearchEventBodyDto } from '../dto/query-search-event-body.dto';
+
+@Injectable()
+export class EventService {
+  constructor(
+    @InjectRepository(Event) private eventRepository: Repository<Event>,
+    private configService: ConfigService,
+    private rsuService: RsuService,
+  ) {}
+
+  /**
+   * Find all event with paging
+   * @param {QueryPagingEventBodyDto} queryEventBody
+   * @returns {Promise<PaginationEventDto>}
+   */
+}
