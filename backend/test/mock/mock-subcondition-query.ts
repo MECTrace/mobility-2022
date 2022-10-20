481a944ac122a4837121e16748eba6858e788660
smlee97 <smlee@pentasecurity.com>
2022-10-20 09:40:57 +0900

    add file : subcondition

---
+++ backend/test/mock/mock-subcondition-query.ts
@@ -0,0 +1,9 @@
+export const mockSubconditionQuery = {
+    where: jest.fn().mockReturnThis(),
+    orWhere: jest.fn().mockReturnThis(),
+    andWhere: jest.fn().mockReturnThis(),
+    whereInIds: jest.fn().mockReturnThis(),
+    andWhereInIds: jest.fn().mockReturnThis(),
+    orWhereInIds: jest.fn().mockReturnThis(),
+  };
+  
