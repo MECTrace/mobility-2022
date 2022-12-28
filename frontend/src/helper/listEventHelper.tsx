98a15ca576d2be6f07671fd9cda35ce3477698a4
smlee97 <smlee@pentasecurity.com>
2022-12-28 16:33:56 +0900

    update functions

--- frontend/src/helper/listEventHelper.tsx
+++ frontend/src/helper/listEventHelper.tsx
@@ -25,37 +25,37 @@ export const renderEventInfo = (data: IListEvent, disableTooltip?: boolean) => {
     case 1: {
       const eventInfoData = data.eventInfo as IEventInfoAvailability;
       const traffic = Math.round((eventInfoData.nic.tx / eventInfoData.nic.rx) * 100);
-      // return (
-      //   <Tooltip
-      //     className="text-ellipsis--2"
-      //     label={`CPU ${eventInfoData.cpu}%, Memory ${eventInfoData.ram}%, Traffic volume ${traffic}%`}
-      //     disabled={disableTooltip}
-      //   >
-      //     CPU (
-      //     <span className={`text-${eventInfoData.cpu < CPUThreshold ? 'green' : 'red'}`}>
-      //       {eventInfoData.cpu}%
-      //     </span>
-      //     ), Memory (
-      //     <span className={`text-${eventInfoData.ram < RAMThreshold ? 'green' : 'red'}`}>
-      //       {eventInfoData.ram}%
-      //     </span>
-      //     ),
-      //     <br />
-      //     Current traffic volume (
-      //     <span className={`text-${traffic < NICThreshold ? 'green' : 'red'}`}>{traffic}%</span>)
-      //   </Tooltip>
-      // );
+      return (
+        <Tooltip
+          className="text-ellipsis--2"
+          label={`CPU ${eventInfoData.cpu}%, Memory ${eventInfoData.ram}%, Traffic volume ${traffic}%`}
+          disabled={disableTooltip}
+        >
+          CPU (
+          <span className={`text-${eventInfoData.cpu < CPUThreshold ? 'green' : 'red'}`}>
+            {eventInfoData.cpu}%
+          </span>
+          ), Memory (
+          <span className={`text-${eventInfoData.ram < RAMThreshold ? 'green' : 'red'}`}>
+            {eventInfoData.ram}%
+          </span>
+          ),
+          <br />
+          Current traffic volume (
+          <span className={`text-${traffic < NICThreshold ? 'green' : 'red'}`}>{traffic}%</span>)
+        </Tooltip>
+      );
     }
     case 2: {
       return (
-      //   <div className="table__event-info text-ellipsis--2">
-      //     <>
-      //       {t('list_event.info.virus', {
-      //         fileName: (data.eventInfo as IEventInfoVirus).fileName || '',
-      //       })}
-      //     </>
-      //   </div>
-      // );
+        <div className="table__event-info text-ellipsis--2">
+          <>
+            {t('list_event.info.virus', {
+              fileName: (data.eventInfo as IEventInfoVirus).fileName || '',
+            })}
+          </>
+        </div>
+      );
     }
     case 3:
       return (
