f8d1bf2ede83001b570f731992b9978256cb46c4
smlee97 <smlee@pentasecurity.com>
2022-12-28 15:00:01 +0900

    update functions

--- frontend/src/helper/hierarchyTreeHelper.ts
+++ frontend/src/helper/hierarchyTreeHelper.ts
@@ -9,6 +9,17 @@ import {
 } from '@/config/hierarchyTreeConfig';
 import { IDiagramData, NodeData } from '@/interface/interfaceHierarchyTree';
 
+/**
+ * Generate Nodes & Edges (Connections) configs for HierarchyTree component to draw a hierarchy tree diagram.
+ * @param {IDiagramData[]} diagramData processed raw flattend diagram data from server.
+ *
+ * @implements How to shows 2-way⚡ connection without using custom node with `<Handler />` or change the order of nodes:
+ * Add `connectionFlow` or just using CSS via `edgeStyle` to **reverse** the animation direction instead.
+ *
+ * `edgeStyle` has higher priority. `edgeStyle` and `connectionFlow` of each "special connection" will override the related common styles of that node.
+ *
+ * @notes The "special" connections are "Edges" with the source id and target id of <Handler /> already defined. [Docs](https://reactflow.dev/docs/guides/custom-nodes/).
+ */
 export const handleDiagramData = (
   diagramData: IDiagramData[],
 ): { initialNodes: Node<NodeData>[]; initialEdges: Edge[] } => {
@@ -34,7 +45,9 @@ export const handleDiagramData = (
     }
 
     if (Array.isArray(eachRecord.source)) {
+      // Multiple Edges/Connections, normal and special connections.
       eachRecord.source.forEach((sourceConnections, index) => {
+        // The first source connection must be a normal connection (parent-child).
         initialEdges.push({
           id: `e${eachRecord.source}-${eachRecord.id}-${index}`,
           source: sourceConnections.id,
@@ -51,6 +64,7 @@ export const handleDiagramData = (
       return;
     }
 
+    // Single normal Edges/Connections
     initialEdges.push({
       id: `e${eachRecord.source}-${eachRecord.id}`,
       source: eachRecord.source,
@@ -62,40 +76,59 @@ export const handleDiagramData = (
   return { initialNodes, initialEdges };
 };
 
+// Handle auto layout/positions for every nodes
 export const processAutoLayoutDiagram = (initialNodes: Node<NodeData>[], initialEdges: Edge[]) => {
-    const skeletonEdges = initialEdges.filter((e) => !e.targetHandle && !e.sourceHandle);
-  
-    const dagreGraph = new dagre.graphlib.Graph(); 
-    dagreGraph.setDefaultEdgeLabel(() => ({}));
-    dagreGraph.setGraph({});
-    initialNodes.forEach((node) => {
-      dagreGraph.setNode(node.id, { width: defaultNodeWidth, height: defaultNodeHeight })
-    });
-  
-    skeletonEdges.forEach((edge) => {
-      dagreGraph.setEdge(edge.source, edge.target);
-  
-    });
-  
-    dagre.layout(dagreGraph);
-  
-    return initialNodes.map((nodeConfig) => {
-      const { x, y } = dagreGraph.node(nodeConfig.id);
-  
-      return {
-        ...nodeConfig,
-        position: { x: x / 1.2, y: y + defaultNodeHeight },
-      };
-    });
-  };
+  const skeletonEdges = initialEdges.filter((e) => !e.targetHandle && !e.sourceHandle);
 
+  const dagreGraph = new dagre.graphlib.Graph(); // compound graph has bugs, since dagrejs is no longer maintained, don't use it.
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+  dagreGraph.setGraph({});
+  initialNodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: defaultNodeWidth, height: defaultNodeHeight });
 
+    // if need to setParent, enable compound graph (compound: true)
+    // if (node.parentNode) {
+    //   dagreGraph.setParent(node.id, node.parentNode);
+    // }
+  });
+
+  skeletonEdges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+
+    // for multigraph:
+    // dagreGraph.setEdge({
+    //   v: edge.source,
+    //   w: edge.target,
+    //   name: `dagre-e${edge.source}-${edge.target}-${index}`,
+    // });
+  });
+
+  dagre.layout(dagreGraph);
+
+  return initialNodes.map((nodeConfig) => {
+    const { x, y } = dagreGraph.node(nodeConfig.id);
+
+    return {
+      ...nodeConfig,
+      position: { x: x / 1.2, y: y + defaultNodeHeight },
+    };
+  });
+};
+
+/**
+ *
+ * @param status node status config
+ * @returns {IStatusConfig | undefined} status config.
+ */
 export const getNodeStatusDataByStatus = (status?: number) => {
-    return NodeStatusConfig.find((statusConfig) => statusConfig.status === status);
-  };
-  
+  return NodeStatusConfig.find((statusConfig) => statusConfig.status === status);
+};
 
-  export const getNodeStatusDataByKey = (statusKey: string) => {
-    return NodeStatusConfig.find((statusConfig) => statusConfig.key === statusKey);
-  };
-  
+/**
+ *
+ * @param statusKey key of node status config
+ * @returns {IStatusConfig | undefined} status config.
+ */
+export const getNodeStatusDataByKey = (statusKey: string) => {
+  return NodeStatusConfig.find((statusConfig) => statusConfig.key === statusKey);
+};
