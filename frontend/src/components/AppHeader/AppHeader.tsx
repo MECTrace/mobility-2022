9b3397d2770d24d67167b7710d0a4d40c8b05da8
smlee97 <smlee@pentasecurity.com>
2022-12-28 16:12:17 +0900

    update error functions

--- frontend/src/components/AppHeader/AppHeader.tsx
+++ frontend/src/components/AppHeader/AppHeader.tsx
@@ -27,19 +27,19 @@ export const AppHeader = () => {
    * Currently support only 2 languages. Use "Select" instead of "ActionIcon" if needed.
    * https://mantine.dev/core/select/
    */
-  const renderToggleLang = languageConfig.map((langConfig) => (
-    <Tooltip key={langConfig.lang} withArrow label={langConfig.tooltip}>
-      <ActionIcon
-        className="header__lang visibility-hidden"
-        size="xl"
-        radius="xl"
-        onClick={() => setToggleLang(langConfig.lang)}
-        hidden={langConfig.lang === toggleLang}
-      >
-        <img src={langConfig.img} alt={langConfig.alt} />
-      </ActionIcon>
-    </Tooltip>
-  ));
+  // const renderToggleLang = languageConfig.map((langConfig) => (
+  //   <Tooltip key={langConfig.lang} withArrow label={langConfig.tooltip}>
+  //     <ActionIcon
+  //       className="header__lang visibility-hidden"
+  //       size="xl"
+  //       radius="xl"
+  //       onClick={() => setToggleLang(langConfig.lang)}
+  //       hidden={langConfig.lang === toggleLang}
+  //     >
+  //       <img src={langConfig.img} alt={langConfig.alt} />
+  //     </ActionIcon>
+  //   </Tooltip>
+  // ));
   
   useEffect(() => {
     i18n.changeLanguage(toggleLang);
